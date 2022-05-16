3af285f4ce6c82ef394c491f9a28fdef15ecc28e
Alan Mendes <adgmendes@hotmail.com>
2022-05-16 19:59:52 -0300

    Adicionao data registration

--- src/Mercadinho.Prateleira.Domain/Estoque.cs
+++ src/Mercadinho.Prateleira.Domain/Estoque.cs
@@ -9,6 +9,7 @@ namespace Mercadinho.Prateleira.Domain
     public class Estoque
     {
         public int Id { get; set; }
+        public Produto Produto { get; set; }
         public int ProdutoId { get; set; }
         public Qualitativo InfoCompra { get; set; }
     }
